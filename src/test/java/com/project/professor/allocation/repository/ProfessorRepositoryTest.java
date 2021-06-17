a14b7b115985a00de32c9b95faf0f27a4626867f
roberto-barros <jroberto.barros2506@gmail.com>
2021-06-17 15:35:34 -0300

    Add ProfessorRepositoryTest

---
+++ src/test/java/com/project/professor/allocation/repository/ProfessorRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.project.professor.allocation.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professor.allocation.entity.Professor;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
+
+public class ProfessorRepositoryTest {
+	
+	@Autowired
+	private ProfessorRepository professorRepository;
+	
+	@Test
+	void test1() {
+		
+		List<Professor> professors = professorRepository.findAll();
+		
+		System.out.println(professors);
+		
+	}
+}
