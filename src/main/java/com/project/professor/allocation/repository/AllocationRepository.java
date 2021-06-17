fa9b3dffd8639313011b47a0975baaa7e8ccd1b3
roberto-barros <jroberto.barros2506@gmail.com>
2021-06-17 17:14:03 -0300

    Homework 5.1 - <Add Custom Queries - Department, Professor, Course, Allocation>

--- src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
@@ -1,11 +1,20 @@
 package com.project.professor.allocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.entity.Course;
+import com.project.professor.allocation.entity.Professor;
 
 @Repository
 public interface AllocationRepository extends JpaRepository<Allocation, Long> {
-
+	
+	List<Allocation> findByFkIdLike(Professor id);
+	
+	List<Allocation> findByFkIdLike(Course id);
+	
+	//é possível fazer um find por parte do nome do professor em allocation?
 }
--- src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
+++ src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
@@ -1,5 +1,7 @@
 package com.project.professor.allocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.project.professor.allocation.entity.Professor;
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
+	//Custom Queries
+	
+	List<Professor> findByNameContainingIgnoreCase(String name);
 }
